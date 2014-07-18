60c7bd3736aa6c14eecdb8fde2de8693d435cd54
hevi <hevi@lut.fi>
2014-07-18 17:32:31 +0300

    reverse sort on name count order

--- pynames/stats.py
+++ pynames/stats.py
@@ -1,14 +1,25 @@
 import logging
 log = logging.getLogger(__name__)
 
+class Item:
+  
+  def __init__(self, name):
+    self.name = name
+    self.count = 0
+
 class Stats:
 
   def __init__(self):
-    self.names = dict()
+    self.names = dict() # name: Item
 
   def add_name(self, name):
     #log.info("name=%s", name)
     assert name is not None
-    self.names[name] = self.names.setdefault(name, 0) + 1
+    if name in self.names:
+      item = self.names[name]
+    else:
+      item = Item(name)
+      self.names[name] = item
+    item.count += 1
 
 
